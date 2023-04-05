7aebc232ee3a11bf8f3d63596975b645c89e7ba2
LeHien2818 <124063932+LeHien2818@users.noreply.github.com>
2023-04-05 20:51:43 +0700

    Add files via upload

---
+++ BT08/BT08_1A.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void func(int x_value)
+{
+   int x;
+   x = x_value;
+   cout << &x << endl;
+}
+void gunc(int y_value)
+{
+   int y;
+   y = y_value;
+   cout << &y;
+
+}
+int main()
+{
+   func(7);
+   gunc(11);
+   return 0;
+}
+
