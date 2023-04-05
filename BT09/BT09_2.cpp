ecad2e518e300f0a09b0c40bcc0c41b18e5d3a9b
LeHien2818 <124063932+LeHien2818@users.noreply.github.com>
2023-04-05 21:02:32 +0700

    Add files via upload

---
+++ BT09/BT09_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int* pointer = new int;
+    int* poin2 = pointer;
+    *pointer = 10;
+    delete pointer;
+    *poin2 = 100;
+    cout << *poin2;
+    delete poin2;
+
+
+}
---
+++ BT09/BT09_3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char* arr = new char[10];
+    char* c = arr + 3;
+    for (int i = 0; i < 9; i++) arr[i] = 'a';
+    arr[9] = '\0';
+    cerr <<"a: " << "-" << arr << "-" << endl;
+    cerr <<"c: " << "-" << c << "-" << endl;
+    delete c;
+    cerr << "a after deleting c:" << "-" << arr << "-" << endl;
+
+    // lỗi vì c không được cấp phát bởi new nên không thể dùng delete
+}
